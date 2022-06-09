ac85017d1843c90a00b90f34c7accf42bc2570c3
Nonso <nonsootoh@gmail.com>
2022-06-09 15:01:13 -0400

    removes unused funcs

--- index.js
+++ index.js
@@ -7,38 +7,12 @@ const splitFunc = (param) => {
   return param.split(",");
 };
 
-const fetchFunc = (tag) => {
-  return axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`);
-};
-
 app.get("/api/ping", (req, res) => {
   res.status(200).json({ success: true });
 });
 
-// app.get("/api/posts", (req, res) => {
-//   const queryParam = req.query;
-
-//   if (!queryParam.tag) {
-//     return res.status(400).json({ error: "Tags parameter is required" });
-//   }
-
-//   axios
-//     .get(`https://api.hatchways.io/assessment/blog/posts?tag=${queryParam.tag}`)
-//     .then((resp) => {
-//       if (resp.data.posts.length > 0) {
-//         res.status(200).json(resp.data);
-//       } else {
-//         res.status(404).json({ error: "page not found" });
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err.message);
-//     });
-// });
-
 app.get("/api/posts", (req, res) => {
   const queryParam = req.query;
-  let respObj = {};
   let respLst = [];
 
   if (!queryParam.tag) {
